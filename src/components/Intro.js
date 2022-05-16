bf3a9363f2bf883754b6f1d36a704b2d3ea74309
abdelrazek <abdelrazekhdd@gmail.com>
2022-05-16 19:06:57 +0200

    intro responsive

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,23 +3,26 @@ import CssShape from "./utilities/CssShape";
 
 export default function Intro() {
   return (
-    <div className="md:flex items-center text-center md:text-left">
-      <div className="flex-1 space-y-5">
-        <h1 className="text-3xl">A Simple Bookmark Manager </h1>
-        <p className="text-grayish-blue">
+    <div className="flex flex-col-reverse	md:flex-row items-center text-center md:text-left lg:space-y-0 space-y-8">
+      <div className="flex-1 space-y-7">
+        <h1 className="lg:text-5xl sm:text-4xl text-3xl font-medium text-very-dark-blue">
+          A Simple Bookmark Manager{" "}
+        </h1>
+        <p className="text-grayish-blue lg:pr-40">
           A clean and simple interface to organize your favourite websites. Open
           a new browser tab and see your sites load instantly. Try it for free.
         </p>
-        <button className="bg-indigo-500 text-white rounded py-2 px-3 mr-2">
-          Get it on Chrome
-        </button>
-        <button className="rounded py-2 px-4 shadow-lg">
-          Get it on Firefox
-        </button>
+        <div className="space-y-2">
+          <button className="bg-indigo-500 text-white rounded  py-2 px-3 mr-2">
+            Get it on Chrome
+          </button>
+          <button className="bg-gray-200 rounded  py-2 px-4 shadow-lg">
+            Get it on Firefox
+          </button>
+        </div>
       </div>
-      <div className="flex-1 ">
-        <img src="/images/illustration-hero.svg" className="relative"></img>
-        <CssShape />
+      <div className="flex-1">
+        <img src="/images/illustration-hero.svg" className=""></img>
       </div>
     </div>
   );
